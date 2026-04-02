7d44c890e5f4337d3cd69efe7b9a1fd3df9ea46f
Peter Dinis <petter1@pobx.sk>
2026-04-02 21:50:38 +0200

    feat: Update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["lucide-react", "next-safe-action", "zod"]
+  transpilePackages: ["lucide-react", "next-safe-action", "zod"],
+  cacheComponents: true,
+  experimental: {
+    cssChunking: true
+  }
 };
 
 export default nextConfig;
